df42e1bbf902e59326a40de50590198bbd198b51
NikitaLapin <nik.lapin2005@gmail.com>
2022-03-23 20:43:05 +0300

    Updated folders hierarchy

--- Assets/Entities/Player/Scripts/Player.cs
+++ Assets/Entities/Player/Scripts/Player.cs
@@ -1,16 +1,26 @@
+using Entities.Player.Movement.StateMachine;
+using Entities.Player.Scripts.Movement.MovementInput;
 using Entities.Player.Scripts.Movement.StateMachine;
-using UnityEditor;
 using UnityEngine;
 
 namespace Entities.Player.Scripts
 {
     [RequireComponent(typeof(PlayerStateMachine))]
+    [RequireComponent(typeof(CharacterInputHandler))]
     public class Player : MonoBehaviour
     {
         private PlayerStateMachine _playerStateMachine;
+        private CharacterInputHandler _inputHandler;
+        
         private void Start()
         {
             _playerStateMachine = GetComponent<PlayerStateMachine>();
+            _inputHandler = GetComponent<CharacterInputHandler>();
+        }
+
+        private void OnNewInput(Vector2 input)
+        {
+            
         }
     }
 }

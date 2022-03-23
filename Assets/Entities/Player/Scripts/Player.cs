d4c5058380cd323b0faee50c412f0d539da23496
NikitaLapin <nik.lapin2005@gmail.com>
2022-03-23 23:26:08 +0300

    Changed hierarchy a bit)

--- Assets/Entities/Player/Scripts/Player.cs
+++ Assets/Entities/Player/Scripts/Player.cs
@@ -1,6 +1,6 @@
+using System;
+using Entities.Player.Input;
 using Entities.Player.Movement.StateMachine;
-using Entities.Player.Scripts.Movement.MovementInput;
-using Entities.Player.Scripts.Movement.StateMachine;
 using UnityEngine;
 
 namespace Entities.Player.Scripts
@@ -9,18 +9,13 @@ namespace Entities.Player.Scripts
     [RequireComponent(typeof(CharacterInputHandler))]
     public class Player : MonoBehaviour
     {
+        [NonSerialized] public CharacterInputHandler InputHandler;
         private PlayerStateMachine _playerStateMachine;
-        private CharacterInputHandler _inputHandler;
-        
+
         private void Start()
         {
             _playerStateMachine = GetComponent<PlayerStateMachine>();
-            _inputHandler = GetComponent<CharacterInputHandler>();
-        }
-
-        private void OnNewInput(Vector2 input)
-        {
-            
+            InputHandler = GetComponent<CharacterInputHandler>();
         }
     }
 }

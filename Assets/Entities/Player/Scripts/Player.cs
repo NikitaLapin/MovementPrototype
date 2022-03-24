8485691fdcd0e883d434e5ca9e56a8b5e3fb3040
NikitaLapin <nik.lapin2005@gmail.com>
2022-03-24 15:55:29 +0300

    Made a code clean up. Updated InputHandler

--- Assets/Entities/Player/Scripts/Player.cs
+++ Assets/Entities/Player/Scripts/Player.cs
@@ -9,13 +9,35 @@ namespace Entities.Player.Scripts
     [RequireComponent(typeof(CharacterInputHandler))]
     public class Player : MonoBehaviour
     {
-        [NonSerialized] public CharacterInputHandler InputHandler;
+        public event Action<Vector2> InputChanged;
+        public event Action<bool> RunChanged;
+        public event Action<bool> JumpChanged;
+
+        private CharacterInputHandler _inputHandler;
         private PlayerStateMachine _playerStateMachine;
 
-        private void Start()
+        private void Awake()
         {
             _playerStateMachine = GetComponent<PlayerStateMachine>();
-            InputHandler = GetComponent<CharacterInputHandler>();
+            _inputHandler = GetComponent<CharacterInputHandler>();
         }
+
+        private void OnEnable()
+        {
+            _inputHandler.InputChanged += OnInputChanged;
+            _inputHandler.JumpChanged += OnJumpChanged;
+            _inputHandler.RunChanged += OnRunChanged;
+        }
+
+        private void OnDisable()
+        {
+            _inputHandler.InputChanged -= OnInputChanged;
+            _inputHandler.JumpChanged -= OnJumpChanged;
+            _inputHandler.RunChanged -= OnRunChanged;
+        }
+
+        private void OnRunChanged(bool isRunning) => RunChanged?.Invoke(isRunning);
+        private void OnJumpChanged(bool isJumped) => JumpChanged?.Invoke(isJumped);
+        private void OnInputChanged(Vector2 newInput) => InputChanged?.Invoke(newInput);
     }
 }
